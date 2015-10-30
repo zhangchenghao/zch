eae2978ac2991ba2dd6c8fb23a7113be3c7835f4
zhangchenghao <tarena@2.local>
2015-10-30 11:26:07 +0800

    das

---
+++ Day17_1_SVN副本/Day17_1_SVN/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Day17_1_SVN
+//
+//  Created by tarena on 15/10/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Day17_1_SVN副本/Day17_1_SVN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day17_1_SVN
+//
+//  Created by tarena on 15/10/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
